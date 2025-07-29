918d0efa5f5b251670ebf2ed8120bae5ad97ed86
youyinnn <youyinnn@gmail.com>
2025-07-29 00:27:08 +0000

    update;

---
+++ cifar_dataset.py
@@ -0,0 +1,81 @@
+from backend_central_dev.data_processing.mix.aug_base import *
+import os
+import json
+from backend_central_dev.data_processing.dataset_utils import (
+    XEraseDataset,
+    NewBasicDataModule,
+    dataclass,
+    field,
+    Type,
+    download_and_extract,
+)
+import sys
+import glob
+import numpy as np
+import pandas as pd
+
+overall_num_classes = 10
+original_dir = ["cifar"]
+class_label_map = {
+    "airplane": 0,
+    "automobile": 1,
+    "bird": 2,
+    "cat": 3,
+    "deer": 4,
+    "dog": 5,
+    "frog": 6,
+    "horse": 7,
+    "ship": 8,
+    "truck": 9
+}
+
+
+@dataclass
+class CIFAR_10_PNG(XEraseDataset):
+
+    num_classes: int = overall_num_classes
+    data_dir: list[str] = field(default_factory=lambda: original_dir)
+
+    class_labels: tuple = field(
+        default_factory=lambda: list(class_label_map.keys()))
+
+    def __check_if_downloaded__(self):
+        return os.path.exists(
+            os.path.join(self.data_dir, "cifar10", "cifar10",
+                         "train", "airplane", "0001.png")
+        )
+
+    def __download_data__(self):
+        os.system(
+            f"kaggle datasets download swaroopkml/cifar10-pngs-in-folders -p {self.data_dir} --unzip"
+        )
+
+    def __check_if_saliency_map_downloaded__(self):
+        return False
+
+    def __x_y_pair_list__(self) -> np.ndarray:
+        train_path_labels = []
+        test_path_labels = []
+        img_paths = glob.glob(self.data_dir + "/**/*.png", recursive=True)
+
+        for p in img_paths:
+            sp = p.split('/')
+            c = sp[-2]
+            if "train" in p:
+                train_path_labels.append([p, class_label_map[c]])
+            else:
+                test_path_labels.append([p, class_label_map[c]])
+
+        return np.array(train_path_labels), np.array(test_path_labels)
+
+    def __download_saliency_map_data__(self):
+        pass
+
+    def __sal_path_transfer__(self, img_path) -> str:
+        return img_path
+
+
+@dataclass
+class CIFAR_10_PNG_NewDataModule(NewBasicDataModule):
+
+    dataset_class: Type = CIFAR_10_PNG
--- mlflow_demo_run.py
+++ mlflow_demo_run.py
@@ -1,4 +1,9 @@
+import json
+import multiprocessing
 import os
+import sys
+import time
+import traceback
 import mlflow
 import numpy as np
 import torch
@@ -65,6 +70,7 @@ class MyLightModel(L.LightningModule):
         loss_fn_hparams: dict = {},
         optimizer_key="Adam",
         optimizer_hparams: dict = {},
+        use_mlflow=True,
         task="multiclass",
         weights=models.EfficientNet_V2_S_Weights.IMAGENET1K_V1
     ):
@@ -103,18 +109,24 @@ class MyLightModel(L.LightningModule):
 
     def forward(self, x):
         return self.model(x)
+    
+    def get_loss(self, x, y, batch_idx):
+        if hasattr(self.model, "get_output_loss"):
+            return self.model.get_output_loss(x, y, self.loss_fn)
+        else:
+            output = self.model(x)
+            loss = self.loss_fn(output, y)
+            return output, loss
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        output = self.model(x)
-        loss = self.loss_fn.to(x.device)(output, y)
+        output, loss = self.get_loss(x, y, batch_idx)
         self.eval_metrics("train", batch, batch_idx, output, loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        output = self.model(x)
-        loss = self.loss_fn.to(x.device)(output, y)
+        output, loss = self.get_loss(x, y, batch_idx)
         self.eval_metrics("val", batch, batch_idx, output, loss)
 
     def configure_optimizers(self):
@@ -164,20 +176,63 @@ class MyLightModel(L.LightningModule):
         metrics[f"{event_key}_loss"] = loss.item()
         self.trainer.progress_bar_metrics["loss"] = metrics[f"{event_key}_loss"]
         self.log_dict(metrics)
-        mlflow.log_metrics(metrics, step=self.trainer.global_step)
+        if self.hparams.use_mlflow:
+            mlflow.log_metrics(metrics, step=self.trainer.global_step)
 
-if __name__ == "__main__":
+
+def task_execution_wrapper(
+    run_name,
+    task_function,
+    task_parameters
+):
+    p = multiprocessing.current_process()
+    task_execution_info = dict(
+        task_parameters=task_parameters,
+        run_name=run_name,
+        pid=p.pid
+    )
+    print(json.dumps(task_execution_info, indent=4))
+
+    original_stdout = sys.stdout
+    original_stderr = sys.stderr
+
+    ticket_log_dir = os.path.join("logs", run_name)
+    os.makedirs(ticket_log_dir, exist_ok=True)
+
+    std_out_file_path = os.path.join(
+        ticket_log_dir, f"out.log")
+    std_err_file_path = os.path.join(
+        ticket_log_dir, f"err.log")
+    std_out_file = open(std_out_file_path, 'w')
+    std_err_file = open(std_err_file_path, 'w')
+
+    try:
+        sys.stdout = std_out_file
+        sys.stderr = std_err_file
+        task_function(run_name, task_parameters)
+        
+    except Exception as e:
+        print(''.join(
+            traceback.TracebackException.from_exception(e).format()),
+            file=sys.stderr)
+        # raise e
+    finally:
+        sys.stdout = original_stdout
+        sys.stderr = original_stderr
+        std_out_file.close()
+        std_err_file.close()
+        with open(std_out_file_path) as f:
+            std_output_str = f.readlines()
+        with open(std_err_file_path) as f:
+            std_error_str = f.readlines()
+
+def task_function_train(run_name, task_parameters):
     L.seed_everything(42)
     mlflow.set_tracking_uri('http://localhost:15001')
-    # device = "mps"
-    device = "cuda"
-    batch_size = 256
-    output_features = 10
 
-    user_home = os.path.expanduser('~')
-    data_root = os.path.join(user_home, 'autodl-tmp',
-                            'ml_data', 'cifar', 'cifar10', 'cifar10')
-
+    batch_size = task_parameters.get('batch_size', 256)
+    device = task_parameters.get('device', 'cuda')
+    
     trainsform = transforms.Compose([
         transforms.Resize((32, 32)),
         # transforms.ToTensor(),
@@ -185,6 +240,10 @@ if __name__ == "__main__":
     
     print("Transform:", trainsform)
 
+    user_home = os.path.expanduser('~')
+    data_root = os.path.join(user_home, 'autodl-tmp',
+                            'ml_data', 'cifar', 'cifar10', 'cifar10')
+    
     train_dataset = MyImageDataset(
         data_root, train=True, transform=trainsform)
     test_dataset = MyImageDataset(
@@ -205,6 +264,7 @@ if __name__ == "__main__":
     )
     
     trainer_params = dict(
+        # max_epochs=5,
         max_epochs=40,
         # max_epochs=1,
         # limit_train_batches=1,
@@ -227,7 +287,7 @@ if __name__ == "__main__":
     )
     
     
-    with mlflow.start_run():
+    with mlflow.start_run(run_name=run_name):
         L.seed_everything(42)
         run = mlflow.active_run()
         # Log training parameters.
@@ -242,12 +302,15 @@ if __name__ == "__main__":
             f.write(str(summary(model)))
         mlflow.log_artifact(model_summary_path)
 
+        st = time.time()
         print("Start train")
         trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=test_dataloader)
+        print("Train finished in", time.time() - st, "seconds")
 
+        st = time.time()
         ckpt_path = trainer.checkpoint_callback.best_model_path
-        
         # ckpt_path = "/root/mlflow_framework_demo/lightning_logs/version_2/checkpoints/best-epoch=39-val_acc=0.8428.ckpt"
+        # ckpt_path = "/root/mlflow_framework_demo/best-epoch=38-val_acc=0.8412.ckpt"
         
         ckpt = torch.load(
             ckpt_path, map_location=device, weights_only=False)
@@ -264,9 +327,11 @@ if __name__ == "__main__":
 
         print("Start val")
         trainer.validate(model, test_dataloader)
+        print("Validation finished in", time.time() - st, "seconds")
 
         print("Skip XAI")
 
+        st = time.time()
         print("XAI Eval")
         model.eval()
         all = []
@@ -282,3 +347,216 @@ if __name__ == "__main__":
         
         print("RCAP mean:", np.array(all).mean())
         mlflow.log_metric('rcap', np.array(all).mean())
+        print("XAI Eval finished in", time.time() - st, "seconds")
+        
+def task_function_train_van(run_name, task_parameters):
+    L.seed_everything(42)
+
+    batch_size = task_parameters.get('batch_size', 256)
+    device = task_parameters.get('device', 'cuda')
+    
+    trainsform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        # transforms.ToTensor(),
+    ])
+    
+    print("Transform:", trainsform)
+
+    user_home = os.path.expanduser('~')
+    data_root = os.path.join(user_home, 'autodl-tmp',
+                            'ml_data', 'cifar', 'cifar10', 'cifar10')
+    
+    train_dataset = MyImageDataset(
+        data_root, train=True, transform=trainsform)
+    test_dataset = MyImageDataset(
+        data_root, train=False, transform=trainsform)
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                shuffle=True, num_workers=0)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
+                                shuffle=False, num_workers=0)
+
+
+    model = MyLightModel(
+        10,
+        optimizer_hparams=dict(
+            lr=0.0001,
+            weight_decay=4.0e-05
+        ),
+        use_mlflow = False
+    )
+    
+    trainer_params = dict(
+        max_epochs=40,
+        callbacks=[
+            L.pytorch.callbacks.ModelCheckpoint(
+                monitor='val_acc',
+                save_top_k=1,
+                filename='best-{epoch}-{val_acc:.4f}',
+                mode='max',
+                save_weights_only=True
+            )
+        ],
+    )
+    
+    trainer = L.Trainer(
+        precision='16-mixed',
+        **trainer_params
+    )
+    
+    
+    L.seed_everything(42)
+
+    st = time.time()
+    print("Start train")
+    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=test_dataloader)
+    print("Train finished in", time.time() - st, "seconds")
+
+    st = time.time()
+    ckpt_path = trainer.checkpoint_callback.best_model_path
+    
+    # ckpt_path = "/root/mlflow_framework_demo/lightning_logs/version_2/checkpoints/best-epoch=39-val_acc=0.8428.ckpt"
+    # ckpt_path = "/root/mlflow_framework_demo/best-epoch=38-val_acc=0.8412.ckpt"
+    
+    ckpt = torch.load(
+        ckpt_path, map_location=device, weights_only=False)
+    
+    model = MyLightModel(
+        10,
+        optimizer_hparams=dict(
+            lr=0.0001,
+            weight_decay=4.0e-05
+        ),
+        use_mlflow = False
+    )
+    
+    model.load_state_dict(ckpt['state_dict'], strict=True)
+
+    print("Start val")
+    trainer.validate(model, test_dataloader)
+    print("Validation finished in", time.time() - st, "seconds")
+
+    print("Skip XAI")
+
+    st = time.time()
+    print("XAI Eval")
+    model.eval()
+    all = []
+    for images, targets in tqdm(test_dataloader):
+        images, targets = images.to(device), targets.to(device)
+        rs = rcap.batch_rcap(
+            model.to(device), (images, targets),
+            gradient_methods.guided_absolute_grad,
+            {}
+        )['overall_rcap']['RCAP']
+        print("RCAP mean:", rs.mean())
+        all.extend(rs)
+    
+    print("Final RCAP mean:", np.array(all).mean())
+    print("XAI Eval finished in", time.time() - st, "seconds")
+
+def task_function_train_mlxops(run_name, task_parameters):
+    L.seed_everything(42)
+    # from cifar_dataset import CIFAR_10_PNG_NewDataModule
+    # from backend_central_dev.model_training.lightning_model import MlxOps_LightningModel, FineTunableModel
+    def model_init_func():
+        return models.efficientnet_v2_s(
+            weights=models.EfficientNet_V2_S_Weights.IMAGENET1K_V1
+        )
+
+
+    def model_fine_tune_func(model, output_features):
+        in_features = model.classifier[-1].in_features
+        model.classifier[-1] = nn.Linear(
+            in_features=in_features, out_features=output_features
+        )
+        
+        return model
+    
+    # dm = CIFAR_10_PNG_NewDataModule(
+    #     img_size=32,
+    #     batch_size=task_parameters.get('batch_size', 256),
+    # )
+    
+    trainsform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        # transforms.ToTensor(),
+    ])
+    
+    print("Transform:", trainsform)
+
+    user_home = os.path.expanduser('~')
+    data_root = os.path.join(user_home, 'autodl-tmp',
+                            'ml_data', 'cifar', 'cifar10', 'cifar10')
+    
+    train_dataset = MyImageDataset(
+        data_root, train=True, transform=trainsform)
+    test_dataset = MyImageDataset(
+        data_root, train=False, transform=trainsform)
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=task_parameters.get('batch_size', 256),
+                                shuffle=True, num_workers=0)
+    test_dataloader = DataLoader(test_dataset, batch_size=task_parameters.get('batch_size', 256),
+                                shuffle=False, num_workers=0)
+    
+    # model = MlxOps_LightningModel(
+    #     FineTunableModel(model_init_func=model_init_func, model_fine_tune_func=model_fine_tune_func),
+    #     output_features=10,
+    #     optimizer_hparams=dict(
+    #         lr=0.0001,
+    #         weight_decay=4.0e-05
+    #     )
+    # )
+    
+    model = MyLightModel(
+        10,
+        optimizer_hparams=dict(
+            lr=0.0001,
+            weight_decay=4.0e-05
+        )
+    )
+    
+    
+    trainer_params = dict(
+        max_epochs=5,
+        callbacks=[
+            L.pytorch.callbacks.ModelCheckpoint(
+                monitor='val_acc',
+                save_top_k=1,
+                filename='best-{epoch}-{val_acc:.4f}',
+                mode='max',
+                save_weights_only=True
+            )
+        ],
+    )
+    
+    trainer = L.Trainer(
+        precision='16-mixed',
+        **trainer_params
+    )
+    
+    print("Start train")
+    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=test_dataloader)
+
+if __name__ == "__main__":
+    
+    multiprocessing.set_start_method('spawn', force=True)
+    process = multiprocessing.Pool(processes=1)
+    
+    args = [
+        # "cifar10-mlflow-demo" + str(time.time()),
+        "cifar10-mlflow-demo-van-time" + str(time.time()),
+        # task_function_train,
+        task_function_train_van,
+        # task_function_train_mlxops,
+        dict(
+            batch_size=256,
+            device='cuda'
+        )
+    ]
+    as_rs = process.apply_async(
+        task_execution_wrapper,
+        args=args
+    )
+    
+    as_rs.wait()
